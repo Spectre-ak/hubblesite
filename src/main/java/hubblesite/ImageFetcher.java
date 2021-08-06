cd538f2d15f41f87d18beec72fdf58352c908626
Spectre-ak <62694340+Spectre-ak@users.noreply.github.com>
2021-08-06 12:46:54 +0530

    Update ImageFetcher.java

--- src/main/java/hubblesite/ImageFetcher.java
+++ src/main/java/hubblesite/ImageFetcher.java
@@ -5,15 +5,33 @@ import java.util.Scanner;
 
 import javax.net.ssl.HttpsURLConnection;
 
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 public class ImageFetcher {
 
 	public static void main(String[] args) throws Exception{
 		// TODO Auto-generated method stub
-		URL url=new URL("https://hubblesite.org/resource-gallery/images");
-		HttpsURLConnection httpsURLConnection=(HttpsURLConnection) url.openConnection();
-		Scanner scanner=new Scanner(httpsURLConnection.getInputStream());
-		while(scanner.hasNextLine()) {
-			System.out.println(scanner.nextLine());
+//		URL url=new URL("https://hubblesite.org/resource-gallery/images");
+//		HttpsURLConnection httpsURLConnection=(HttpsURLConnection) url.openConnection();
+//		Scanner scanner=new Scanner(httpsURLConnection.getInputStream());
+//		while(scanner.hasNextLine()) {
+//			System.out.println(scanner.nextLine());
+//		}
+//		
+		
+		Document document=Jsoup.connect("https://hubblesite.org/resource-gallery/images?filterUUID=4c394bbb-b21e-43ab-a160-2a4521d70243&page=1&itemsPerPage=600&").get();
+	
+		System.out.println(document.title());
+		
+		Elements elements=document.getElementsByClass("col-sm-4");
+		System.out.println(elements.size());
+		
+		for(Element element:elements) {
+			System.out.println(element.getElementsByClass("text-overlay__center").get(0).text());
 		}
 	}
 
