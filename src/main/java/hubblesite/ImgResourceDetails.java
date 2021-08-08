c7bb8feb625b41f520972a91e015f347bcc0139d
Spectre-ak <62694340+Spectre-ak@users.noreply.github.com>
2021-08-08 19:45:55 +0530

    Update ImgResourceDetails.java

--- src/main/java/hubblesite/ImgResourceDetails.java
+++ src/main/java/hubblesite/ImgResourceDetails.java
@@ -5,13 +5,19 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class ImgResourceDetails {
-	Document document;
+	private Document document;
+	private Element detailRow;
 	public ImgResourceDetails(Document document) {
 		this.document=document;
+		detailRow=document.getElementsByClass("resource-gallery-detail").get(0);
 	}
 	
 	public String getImageTitle() {
-		Element element=document.getElementsByClass("section-header__title").get(0);
+		Element element=this.document.getElementsByClass("section-header__title").get(0);
 		return element.text();
+	}
+	
+	public String getImageReleaseDate() {
+		return this.detailRow.getElementsByClass("col-md-4").get(0).text();
 	}
 }
