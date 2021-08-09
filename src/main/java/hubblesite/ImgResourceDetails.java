f4e90f9d12497c25b29309c48c52b9c19476a10b
Spectre-ak <62694340+Spectre-ak@users.noreply.github.com>
2021-08-09 10:41:38 +0530

    Update ImgResourceDetails.java

--- src/main/java/hubblesite/ImgResourceDetails.java
+++ src/main/java/hubblesite/ImgResourceDetails.java
@@ -1,5 +1,8 @@
 package hubblesite;
 
+import java.awt.List;
+import java.util.ArrayList;
+
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
@@ -20,5 +23,15 @@ public class ImgResourceDetails {
 	public String getImageReleaseDate() {
 		return this.detailRow.getElementsByClass("col-md-4").get(0)
 				.getElementsByTag("p").get(0).text();
+	}
+	public ArrayList<String> getImageTags(){
+		Elements elements=this.detailRow.getElementsByClass("col-md-8").get(0)
+				.getElementsByClass("keyword-tag");
+		ArrayList<String> keywordTags=new ArrayList<String>();
+		
+		for(Element element:elements) {
+			keywordTags.add(element.text());
+		}
+		return keywordTags;
 	}
 }
