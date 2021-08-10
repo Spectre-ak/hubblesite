82676d0edf3e2f00207a6229b33f429bcf038f02
Spectre-ak <62694340+Spectre-ak@users.noreply.github.com>
2021-08-10 12:25:26 +0530

    Create VideoRescDetails.java

---
+++ src/main/java/hubblesite/VideoRescDetails.java
@@ -0,0 +1,43 @@
+package hubblesite;
+
+import java.util.ArrayList;
+
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class VideoRescDetails {
+
+	private Document document;
+	public VideoRescDetails(Document document) {
+		this.document=document;
+	}
+
+	public ArrayList<String[]> getDownloadOps(){
+		
+		
+		return null;
+	}
+	
+	public String getTitle() {
+		return this.document.getElementsByClass("section-header__title").get(0).text().strip();
+
+	}
+	
+	
+	public ArrayList<String> getVideoTags() {
+		Elements keywordTags=document.getElementsByClass("keyword-tag");
+		ArrayList<String> tagsArrayList=new ArrayList<String>();
+		
+		for(Element element:keywordTags) {
+			tagsArrayList.add(element.text());
+		}
+		return tagsArrayList;
+	}
+	
+	public String getVideoReleaseDate() {
+		return this.document.getElementsByClass("col-md-4").get(0)
+				.getElementsByTag("p").get(0).text().strip();
+	}
+}
+
